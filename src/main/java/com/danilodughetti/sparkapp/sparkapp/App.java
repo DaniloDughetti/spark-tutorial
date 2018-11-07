198b970fb3a7f2a63d756fbaf456909a64355f6a
unknown <danilo.dughetti@gmail.com>
2018-11-07 18:29:39 +0100

    init

---
+++ src/main/java/com/danilodughetti/sparkapp/sparkapp/App.java
@@ -0,0 +1,13 @@
+package com.danilodughetti.sparkapp.sparkapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
